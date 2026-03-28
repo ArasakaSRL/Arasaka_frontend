88551dbd3e98b047232e767d2f55c09698337879
giuli <giulianaqs818@gmail.com>
2026-03-27 23:27:12 -0400

    primer commit

---
+++ src/features/proyectos/pages/PageProyectos.tsx
@@ -0,0 +1,19 @@
+export default function PageProyectos() {
+    return (
+    <div className="px-8 space-y-6">
+      <div className="flex justify-between items-center">
+        <div>
+          <h3 className="text-lg text-left">Proyectos</h3>
+          <p className="text-sm text-left">Gestiona tus proyectos de software</p>
+        </div>
+            <button className="flex items-center gap-2 px-4 py-2 rounded-lg bg-[#1B2A6D] text-white hover:bg-[#27496E]">
+              Nuevo Proyecto
+            </button>
+        </div>
+
+        <div className="border p-10">
+          lol
+        </div>
+    </div>
+    )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,7 +5,9 @@ import App from '../App';
 const Home = lazy(() => import('../pages/Home'));
 const About = lazy(() => import('../pages/About'));
 const NotFound = lazy(() => import('../pages/NotFound'));
+const PageProyectos = lazy(() => import('../features/proyectos/pages/PageProyectos'));
 
+// eslint-disable-next-line react-refresh/only-export-components
 const SuspenseWrapper = ({ children }: { children: React.ReactNode }) => (
   <Suspense fallback={<section id="center"><p>Cargando vista...</p></section>}>
     {children}
@@ -22,7 +24,8 @@ export const router = createBrowserRouter([
         index: true, 
         element: (
           <SuspenseWrapper>
-            <Home />
+            {/* <Home /> */}
+            <PageProyectos />
           </SuspenseWrapper>
         ),
       },
