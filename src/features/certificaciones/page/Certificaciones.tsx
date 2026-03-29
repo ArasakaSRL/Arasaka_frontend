1afe671e97370fbcf93e16676e492520f2422a79
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2026-03-28 22:33:33 -0400

    feat: Componentes cards categorias

--- src/features/certificaciones/page/Certificaciones.tsx
+++ src/features/certificaciones/page/Certificaciones.tsx
@@ -1,36 +1,51 @@
 import { Banner } from "../components/BannerCertificaciones";
+import { CategoriaCard } from "../components/CategoriaCard";
 import { FechaInput } from "../components/FechaInput";
 import { ImagenUploader } from "../components/ImagenUploader";
 import { InputCertificaciones } from "../components/InputCertificaciones";
 
 export function Certificaciones(){
-    return(
-        <section>
-             <div className="p-1 space-y-6">
+  return(
+    <section>
+      <div className="p-1 space-y-6">
 
-                <Banner />
+        <Banner />
 
-                <div>
-                    <h3 className="text-sm text-dark-500 font-medium-ui mb-3 text-left">
-                    Categorías
-                    </h3>
+        <div>
+          <h3 className="text-sm text-dark-500 font-medium-ui mb-3 text-left">
+            Categorías
+          </h3>
 
-                    {/*<CategoryAddCard />*/}
-                </div>
+          <CategoriaCard
+            title="Académico"
+            description="Diplomas, logros educativos, etc"
+            image="https://images.unsplash.com/photo-1523240795612-9a054b0db644"
+          />
 
-                <h2 className="text-center text-lg text-dark-500 tracking-widest font-semibold-ui">
-                    CERTIFICACIONES
-                </h2>
-                </div>
-                <InputCertificaciones tamMax={20} placeHolder="" height={40} ></InputCertificaciones>
-                <InputCertificaciones 
-                    tamMax={100} 
-                    placeHolder="" 
-                    width={300} 
-                    height={100}>
-                </InputCertificaciones>
-                <FechaInput></FechaInput>
-                <ImagenUploader></ImagenUploader>
-        </section>
-    );
+          <CategoriaCard
+            title="Idiomas"
+            description="Diplomas y certificados, etc"
+            image="https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+          />
+        </div>
+
+        <h2 className="text-center text-lg text-dark-500 tracking-widest font-semibold-ui">
+          CERTIFICACIONES
+        </h2>
+
+        <InputCertificaciones tamMax={20} placeHolder="" height={40} />
+
+        <InputCertificaciones 
+          tamMax={100} 
+          placeHolder="" 
+          width={300} 
+          height={100}
+        />
+
+        <FechaInput />
+        <ImagenUploader />
+
+      </div>
+    </section>
+  );
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,11 @@
+import { InputCertificaciones } from "../features/certificaciones/components/InputCertificaciones";
+
 export default function About() {
   return (
     <section id="center">
       <h1>Acerca de nosotros</h1>
       <p>Esta es una página adicional para probar React Router.</p>
+      <InputCertificaciones tamMax={20} placeHolder="" height={40} ></InputCertificaciones>
     </section>
   )
 }
