b2ab91b115c249ac02a900c5c32ac2ade23eb302
giuli <giulianaqs818@gmail.com>
2026-06-01 23:11:51 -0400

    arregloo

--- src/features/users/components/UsuariosBuscador.tsx
+++ src/features/users/components/UsuariosBuscador.tsx
@@ -16,7 +16,7 @@ export const UserSearch = ({
         className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"
       />
 
-      <input value={value} onChange={e => onChange(e.target.value)} placeholder="Buscar talento o username..."
+      <input value={value} onChange={e => onChange(e.target.value)} placeholder="Buscar nombre o nombre de usuario..."
         className="w-full rounded-xl border text-sm border-gray-200 bg-white py-2 pl-11 pr-4 outline-none focus:border-blue-500"
       />
     </div>
