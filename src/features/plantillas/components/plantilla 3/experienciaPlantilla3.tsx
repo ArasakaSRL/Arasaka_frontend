be2a8549a3b3ce2340470cfc056ccb24e9078889
giuli <giulianaqs818@gmail.com>
2026-06-08 01:32:46 -0400

    detaller experiencias

--- src/features/plantillas/components/plantilla 3/experienciaPlantilla3.tsx
+++ src/features/plantillas/components/plantilla 3/experienciaPlantilla3.tsx
@@ -18,7 +18,6 @@ export default function Experiencia({
 };
   return (
     <section className="bg-white border-4 border-slate-900 rounded-[36px] p-6 md:p-8 shadow-[14px_14px_0px_0px_rgba(247,214,224,1)] space-y-6">
-      {/* Header */}
       <div className="flex items-center gap-2 border-b-2 border-slate-100 pb-4">
         <div className="w-8 h-8 rounded-full bg-[#7bdff2] border-2 border-slate-900 flex items-center justify-center">
           <LayoutPanelLeft size={20} color="black"/>
@@ -27,8 +26,7 @@ export default function Experiencia({
           Experiencia
         </h3>
       </div>
-      {/* Timeline */}
-      <div className="space-y-6 relative before:absolute before:left-4.25 before:top-2 before:bottom-2 before:w-[3px] before:bg-slate-900">
+      <div className="space-y-6 relative before:absolute before:left-4.25 before:top-2 before:bottom-2 before:w-0.75 before:bg-slate-900">
         {experiencias.map((exp, idx) => {
           const fechaFin =
             exp.fecha_fin ??
@@ -39,10 +37,9 @@ export default function Experiencia({
               key={exp.id_experiencia}
               className="relative pl-10 group"
             >
-              {/* Nodo */}
               <span
                 className={`
-                  absolute left-[10px] top-1
+                  absolute left-2.5 top-1
                   w-4 h-4 rounded-full
                   border-2 border-slate-900
                   z-10 transition
@@ -54,7 +51,7 @@ export default function Experiencia({
                   }
                 `}
               />
-              {/* Card */}
+              {/*card*/}
               <div className="bg-[#eff7f6]/40 hover:bg-[#eff7f6]/95 border-2 border-slate-900 p-5 px-8 rounded-3xl transition duration-300 flex flex-col md:flex-row gap-4 justify-between">
                 <div className="space-y-1 md:w-1/3 text-left self-start">
                   <span className="text-[9px] font-black uppercase text-slate-400 block tracking-wider">
@@ -63,7 +60,7 @@ export default function Experiencia({
                   <h4 className="font-black text-base text-slate-900">
                     {exp.cargo}
                   </h4>
-                  <p className="text-xs text-[#f2b5d4] font-black">
+                  <p className="text-sm text-[#f2b5d4] font-black">
                     {exp.Nombre_empresa}
                   </p>
                   <span className="inline-flex gap-1 text-[9px] font-black text-slate-900 bg-white border border-slate-900 px-2.5 py-1 rounded-full uppercase mt-1">
@@ -72,7 +69,7 @@ export default function Experiencia({
                   </span>
                 </div>
                 <div className="md:w-2/3 flex items-center">
-                  <p className="text-xs text-left text-slate-700 leading-relaxed font-medium">
+                  <p className="text-xs text-left sm:text-xs md:text-sm text-slate-700 leading-relaxed font-medium">
                     {exp.descripcion}
                   </p>
                 </div>
