92a6925da0648c058afd71b4d97e0de72b92ef18
giuli <giulianaqs818@gmail.com>
2026-06-07 22:32:15 -0400

    secci habilidades

--- src/features/plantillas/components/plantilla 3/habilidadesPlantilla3.tsx
+++ src/features/plantillas/components/plantilla 3/habilidadesPlantilla3.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { Award, Smile, Layers } from "lucide-react"; 
 import type {
   HabilidadTecnica,
   HabilidadBlanda,
@@ -71,19 +71,22 @@ export default function HabilidadesJam({
 
   return (
     <section className="bg-white border-4 border-slate-900 rounded-[36px] p-6 md:p-8 shadow-[14px_14px_0px_0px_rgba(178,247,239,1)]  space-y-6">
+      
       <div className="flex flex-col sm:flex-row items-center justify-between gap-4 border-b-2 border-slate-200 pb-4">
         <div className="flex items-center gap-2">
           <div className="w-8 h-8 rounded-full bg-[#f2b5d4] border-2 border-slate-900 flex items-center justify-center font-black">
-            ★
+            <Award size={16} color="#1e293b" />
           </div>
-          <h2 className="font-black text-xl uppercase tracking-tight">
+          <h2 className="font-black text-xl tracking-tight text-slate-900 uppercase">
             Habilidades
           </h2>
         </div>
+        
         <div className="bg-[#eff7f6] p-1.5 rounded-2xl border-2 border-slate-900 flex gap-2">
           <button
             onClick={() => setTipo("tech")}
             className={`
+              flex items-center gap-1
               px-4 py-2 rounded-xl text-xs font-black uppercase transition-all
               ${
                 tipo === "tech"
@@ -92,11 +95,13 @@ export default function HabilidadesJam({
               }
             `}
           >
-            🛠 Técnicas
+            <Layers size={16} color="#1e293b" />
+            Técnicas
           </button>
           <button
             onClick={() => setTipo("soft")}
             className={`
+              flex items-center gap-1
               px-4 py-2 rounded-xl text-xs font-black uppercase transition-all
               ${
                 tipo === "soft"
@@ -105,63 +110,67 @@ export default function HabilidadesJam({
               }
             `}
           >
-            ✨ Blandas
+             <Smile size={16} color="#1e293b" />
+             Blandas
           </button>
         </div>
       </div>
+
       {tipo === "tech" ? (
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4">
-          {tecnicas.map((skill) => {
-            const styles = getLevelStyles(skill.nivel);
-            return (
-              <div
-                key={skill.id_habilidad}
-                className=" group bg-[#eff7f6] p-4 rounded-3xl border-2 border-slate-900 hover:bg-white hover:-translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(15,23,42,1)] transition-all duration-300 "
-              >
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 gap-4">
+{tecnicas.map((skill) => {
+  const styles = getLevelStyles(skill.nivel);
 
-                <div className="flex items-center gap-3">
-                  <div className=" w-12 h-12 rounded-full bg-[#c2e0e6] border-2 border-slate-900 flex items-center justify-center shrink-0 overflow-hidden ">
-                    {skill.tecnologias?.[0]?.logo ? (
-                      <img
-                        src={skill.tecnologias[0].logo}
-                        alt={skill.nombre ?? ""}
-                        className="w-7 h-7 object-contain"
-                      />
-                    ) : (
-                      <span className="font-black text-xs">
-                        UI
-                      </span>
-                    )}
-                  </div>
+  return (
+    <div
+      key={skill.id_habilidad}
+      className=" bg-[#eff7f6] border-2 border-slate-900 rounded-[28px] p-3 hover:bg-white hover:-translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(15,23,42,1)] transition-all duration-300" >
+      <div className="flex flex-col xl:flex-row items-center gap-3">
 
-                  <div className="flex-1 min-w-0">
-                    <h4 className="font-black text-sm text-slate-900 truncate">
-                      {skill.nombre}
-                    </h4>
-                  </div>
-                </div>
-                <div className="mt-3">
-                  <div className="w-full bg-slate-200 h-2 rounded-full overflow-hidden border border-slate-900">
-                    <div
-                      className={`h-full rounded-full ${styles.bg}`}
-                      style={{
-                        width: getWidth(skill.nivel),
-                      }}
-                    />
+        {/* Logo */}
+        <div
+          className=" w-14 h-14 md:w-16 md:h-16 rounded-full bg-[#c2e0e6] border-2 border-slate-900 flex items-center justify-center shrink-0 overflow-hidden">
+          {skill.tecnologias?.[0]?.logo ? (
+            <img
+              src={skill.tecnologias[0].logo}
+              alt={skill.nombre ?? ""}
+              className="w-8 h-8 md:w-10 md:h-10 object-contain"
+            />
+          ) : (
+            <span className="font-black text-sm">
+              {skill.nombre?.charAt(0)}
+            </span>
+          )}
+        </div>
 
-                  </div>
-                  <span
-                    className={`text-[10px] font-black uppercase mt-1 block ${styles.text}`}
-                  >
-                    {skill.nivel}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+        {/* Información */}
+        <div className="flex-1 w-full">
+          <h4 className=" font-black text-xs md:text-sm text-center xl:text-left leading-tight wrap-break-word mb-2">
+            {skill.nombre}
+          </h4>
+
+          <div className="w-full h-2 bg-slate-200 rounded-full border border-slate-900 overflow-hidden">
+            <div
+              className={`h-full rounded-full ${styles.bg}`}
+              style={{
+                width: getWidth(skill.nivel),
+              }}
+            />
+          </div>
+
+          <span className={` text-[9px] md:text-[10px] font-black uppercase mt-1 block text-center xl:text-left ${styles.text}`}>
+            {skill.nivel}
+          </span>
+
+        </div>
+
+      </div>
+    </div>
+  );
+})}
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-3 gap-3">
           {blandas.map((skill) => {
             const styles = getLevelStyles(skill.nivel);
             return (
@@ -173,7 +182,7 @@ export default function HabilidadesJam({
                   <span
                     className=" w-3.5 h-3.5 rounded-full bg-[#f2b5d4] border border-slate-900 "
                   />
-                  <span className="font-bold text-xs text-slate-900">
+                  <span className="font-bold text-xs text-slate-900 wrap-break-word text-left">
                     {skill.nombre}
                   </span>
                 </div>
