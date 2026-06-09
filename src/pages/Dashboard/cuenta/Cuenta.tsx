6cbb405ad05678a486d70913e06285ef0369a9de
Jhaseft <jhasesaat@gmail.com>
2026-06-08 23:53:45 -0400

    cambios para emails

--- src/pages/Dashboard/cuenta/Cuenta.tsx
+++ src/pages/Dashboard/cuenta/Cuenta.tsx
@@ -13,10 +13,7 @@ export default function Cuenta() {
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 items-start">
 
                 <div className="lg:col-span-12 flex items-center justify-between gap-4">
-                    <div className="min-w-0">
-                        <h1 className="text-h-sm! text-left font-bold text-slate-800 leading-tight">Mi Cuenta</h1>
-                        <p className="text-sm sm:text-sm text-left text-slate-500 mt-0.5">Gestiona tu información personal y seguridad</p>
-                    </div>
+
                     <button
                         onClick={() => navigate('/Dashboard/perfil/General')}
                         className="flex items-center gap-1.5 px-3 py-2 sm:px-4 rounded-xl border border-slate-200 bg-white text-slate-600 text-xs sm:text-sm font-medium hover:bg-slate-50 hover:border-slate-300 transition-all shadow-sm shrink-0 whitespace-nowrap"
