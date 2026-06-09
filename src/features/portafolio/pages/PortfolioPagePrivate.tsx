03d5da9a481784e408e5395c34dfe2982a4773db
giuli <giulianaqs818@gmail.com>
2026-06-09 03:10:01 -0400

    arreglo vista previa

--- src/features/portafolio/pages/PortfolioPagePrivate.tsx
+++ src/features/portafolio/pages/PortfolioPagePrivate.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import PortfolioHeader from '../components/PortfolioHeader '; 
+// import PortfolioHeader from '../components/PortfolioHeader '; 
 import { getPortafolioPrivate } from '../lib/portafolio.service';
 import type { Usuario ,habilidades,experiencias,HabilidadTecnica,HabilidadBlanda,Proyectos ,configuracion,certificaciones} from '../types/portafolioType';
-import HabilidadesTecnicas from '@/features/portafolio/components/HabilidadesTecnicas';
-import ExperienceTimeline from '../components/ExperienceTimeline';
-import HabilidadesBlandas from '../components/HabilidadesBlandas';
-import SeccionProyectos from '../components/SeccionProyectos';
-import { NavbarVertical } from '../components/NavbarVertical';
-import { CertificacionesSection } from '../components/CertificacionesSection';
-import { set } from 'zod';
+// import HabilidadesTecnicas from '@/features/portafolio/components/HabilidadesTecnicas';
+// import ExperienceTimeline from '../components/ExperienceTimeline';
+// import HabilidadesBlandas from '../components/HabilidadesBlandas';
+// import SeccionProyectos from '../components/SeccionProyectos';
+// import { NavbarVertical } from '../components/NavbarVertical';
+// import { CertificacionesSection } from '../components/CertificacionesSection';
+import Predeterminada from '@/features/plantillas/pages/Predeterminada';
+import Plantilla1 from '@/features/plantillas/pages/Plantilla1';
+import Plantilla2 from '@/features/plantillas/pages/Plantilla2';
+import Plantilla3 from '@/features/plantillas/pages/Plantilla3';
 
 export default function PortfolioPage() {
     const { slug } = useParams<{ slug: string }>();
@@ -58,48 +61,20 @@ export default function PortfolioPage() {
         return <div className="min-h-screen bg-[#0a1120] flex items-center justify-center text-white">No se encontró el usuario</div>;
     }
 
-    return (
-             
-        <div className="p-3 w-full min-h-screen ">
-                <NavbarVertical /> 
-            <div className="max-w-350 mx-auto flex flex-col gap-6">
-                <section id="inicio">
-                    <PortfolioHeader
-                        usuario={usuario}
-                        proyectos={configuracion?.mostrar_proyectos ? proyectos : []}
-                        tecnicas={configuracion?.mostrar_habilidades ? habilidadesTecnicas : []}
-                        blandas={configuracion?.mostrar_habilidades ? habilidadesBlandas : []}
-                        experiencias={configuracion?.mostrar_experiencias ? experiencias : []}
-                        certificaciones={configuracion?.mostrar_certificaciones ? certificaciones : []}
-                        mostrarCV={configuracion?.mostrar_cv ?? true}
-                        mostrarContacto={configuracion?.mostrar_contacto ?? true}
-                        mostrarRedes={configuracion?.mostrar_redes_profesionales ?? true}
-                    />
-                </section>
-            </div>
-            {configuracion?.mostrar_habilidades && (
-                <section id="habilidades">
-                    <HabilidadesTecnicas tecnicas={habilidadesTecnicas} />
-                    <HabilidadesBlandas blandas={habilidadesBlandas} />
-                </section>
-           
-               )}
-                {configuracion?.mostrar_experiencias && (
-                 <section id="experiencia">
-                    <ExperienceTimeline experiencias={experiencias} />
-                 </section>
-                )}
-                    {configuracion?.mostrar_proyectos && (
-                  <section id="proyectos">
-                    <SeccionProyectos proyectos={proyectos} />
-                  </section>
-                )}
-                {configuracion?.mostrar_certificaciones && (
-                    <section id="certificaciones">
-                    <CertificacionesSection certificaciones={certificaciones} />
-                    </section>
-                )}
-               
-        </div>
-    );
+    const plantilla = configuracion?.plantilla ?? "predeterminado";
+
+    switch (plantilla) {
+    case "minimalista":
+        return <Plantilla1 />;
+
+    case "profesional":
+        return <Plantilla2 />;
+
+    case "stiloPastel":
+        return <Plantilla3 />;
+
+    case "predeterminado":
+    default:
+        return <Predeterminada />;
+    }
 }
