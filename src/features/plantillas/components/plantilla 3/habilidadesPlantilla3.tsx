1ed918919e896e5ee9554458e06c77f04f19ddf8
giuli <giulianaqs818@gmail.com>
2026-06-15 00:21:41 -0400

    mejorar plantilla cute

--- src/features/plantillas/components/plantilla 3/habilidadesPlantilla3.tsx
+++ src/features/plantillas/components/plantilla 3/habilidadesPlantilla3.tsx
@@ -65,7 +65,7 @@ export default function HabilidadesJam({
         };
     }
   };
-
+  const usarCarrusel = tecnicas.length > 5;
   return (
     <section className="bg-white border-4 border-slate-900 rounded-[36px] p-6 md:p-8 shadow-[14px_14px_0px_0px_rgba(178,247,239,1)]  space-y-6">
       
@@ -112,58 +112,117 @@ export default function HabilidadesJam({
       </div>
 
       {tipo === "tech" ? (
-         <SkillsCarousel>
-           {tecnicas.map((skill) => {
-            const styles = getLevelStyles(skill.nivel);
+        usarCarrusel ? (
+          <SkillsCarousel>
+            {tecnicas.map((skill) => {
+              const styles = getLevelStyles(skill.nivel);
 
-            return (
-              <div
-                key={skill.id_habilidad}
-                className=" bg-[#eff7f6] border-2 border-slate-900 rounded-[28px] p-3 hover:bg-white hover:-translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(15,23,42,1)] transition-all duration-300" >
-                <div className="flex flex-col xl:flex-row items-center gap-3">
+              return (
+                <div
+                  key={skill.id_habilidad}
+                  className=" bg-[#eff7f6] border-2 border-slate-900 rounded-[28px] p-3 hover:bg-white hover:-translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(15,23,42,1)] transition-all duration-300"
+                >
+                  <div className="flex flex-col xl:flex-row items-center gap-3">
 
-                  {/* Logo */}
-                  <div
-                    className=" w-14 h-14 md:w-16 md:h-16 rounded-full bg-[#c2e0e6] border-2 border-slate-900 flex items-center justify-center shrink-0 overflow-hidden">
-                    {skill.tecnologias?.[0]?.logo ? (
-                      <img
-                        src={skill.tecnologias[0].logo}
-                        alt={skill.nombre ?? ""}
-                        className="w-8 h-8 md:w-10 md:h-10 object-contain"
-                      />
-                    ) : (
-                      <span className="font-black text-sm ">
-                        {skill.nombre?.charAt(0)}
-                      </span>
-                    )}
-                  </div>
-
-                  {/* Información */}
-                  <div className="flex-1 w-full">
-                    <h4 className="text-black font-black text-xs md:text-sm text-center xl:text-left leading-tight wrap-break-word mb-2 hidden md:block">
-                      {skill.nombre}
-                    </h4>
-
-                    <div className="w-full h-2 bg-slate-200 rounded-full border border-slate-900 overflow-hidden hidden md:block">
-                      <div
-                        className={`h-full rounded-full ${styles.bg}`}
-                        style={{
-                          width: getWidth(skill.nivel),
-                        }}
-                      />
+                    {/* Logo */}
+                    <div
+                      className=" w-14 h-14 md:w-16 md:h-16 rounded-full bg-[#c2e0e6] border-2 border-slate-900 flex items-center justify-center shrink-0 overflow-hidden"
+                    >
+                      {skill.tecnologias?.[0]?.logo ? (
+                        <img
+                          src={skill.tecnologias[0].logo}
+                          alt={skill.nombre ?? ""}
+                          className="w-8 h-8 md:w-10 md:h-10 object-contain"
+                        />
+                      ) : (
+                        <span className="font-black text-sm ">
+                          {skill.nombre?.charAt(0)}
+                        </span>
+                      )}
                     </div>
 
-                    <span className={` text-[9px] md:text-[10px] font-black uppercase mt-1 block text-center xl:text-left ${styles.text}`}>
-                      {skill.nivel}
-                    </span>
+                    {/* Información */}
+                    <div className="flex-1 w-full">
+                      <h4 className="text-black font-black text-xs md:text-sm text-center xl:text-left leading-tight wrap-break-word mb-2 hidden md:block">
+                        {skill.nombre}
+                      </h4>
+
+                      <div className="w-full h-2 bg-slate-200 rounded-full border border-slate-900 overflow-hidden hidden md:block">
+                        <div
+                          className={`h-full rounded-full ${styles.bg}`}
+                          style={{
+                            width: getWidth(skill.nivel),
+                          }}
+                        />
+                      </div>
+
+                      <span className={` text-[9px] md:text-[10px] font-black uppercase mt-1 block text-center xl:text-left ${styles.text}`}>
+                        {skill.nivel}
+                      </span>
+
+                    </div>
 
                   </div>
-
                 </div>
-              </div>
-            );
-          })}
-         </SkillsCarousel>
+              );
+            })}
+          </SkillsCarousel>
+        ) : (
+          <div className="grid grid-cols-3 md:grid-cols-5 gap-3">
+            {tecnicas.map((skill) => {
+              const styles = getLevelStyles(skill.nivel);
+
+              return (
+                <div
+                  key={skill.id_habilidad}
+                  className=" bg-[#eff7f6] border-2 border-slate-900 rounded-[28px] p-3 hover:bg-white hover:-translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(15,23,42,1)] transition-all duration-300"
+                >
+                  <div className="flex flex-col xl:flex-row items-center gap-3">
+
+                    {/* Logo */}
+                    <div
+                      className=" w-14 h-14 md:w-16 md:h-16 rounded-full bg-[#c2e0e6] border-2 border-slate-900 flex items-center justify-center shrink-0 overflow-hidden"
+                    >
+                      {skill.tecnologias?.[0]?.logo ? (
+                        <img
+                          src={skill.tecnologias[0].logo}
+                          alt={skill.nombre ?? ""}
+                          className="w-8 h-8 md:w-10 md:h-10 object-contain"
+                        />
+                      ) : (
+                        <span className="font-black text-sm ">
+                          {skill.nombre?.charAt(0)}
+                        </span>
+                      )}
+                    </div>
+
+                    {/* Información */}
+                    <div className="flex-1 w-full">
+                      <h4 className="text-black font-black text-xs md:text-sm text-center xl:text-left leading-tight wrap-break-word mb-2 hidden md:block">
+                        {skill.nombre}
+                      </h4>
+
+                      <div className="w-full h-2 bg-slate-200 rounded-full border border-slate-900 overflow-hidden hidden md:block">
+                        <div
+                          className={`h-full rounded-full ${styles.bg}`}
+                          style={{
+                            width: getWidth(skill.nivel),
+                          }}
+                        />
+                      </div>
+
+                      <span className={` text-[9px] md:text-[10px] font-black uppercase mt-1 block text-center xl:text-left ${styles.text}`}>
+                        {skill.nivel}
+                      </span>
+
+                    </div>
+
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-3 gap-3">
           {blandas.map((skill) => {
